e5e7d3116af7657699b7aeb0b7fce6b3b4be47c4
SuchitaPal <62982555+Suchita-wrk@users.noreply.github.com>
2020-04-30 11:44:34 +0530

    third commit

--- spring-loose-coupling/src/com/spring/Client.java
+++ spring-loose-coupling/src/com/spring/Client.java
@@ -1,6 +1,7 @@
 package com.spring;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
@@ -8,7 +9,7 @@ public class Client {
 		ApplicationContext context= new ClassPathXmlApplicationContext("beans.xml");
 		Student var = context.getBean("student",Student.class);
 		var.cheating();
-		
+		((AbstractApplicationContext) context).close();
 	
 	}
 }
