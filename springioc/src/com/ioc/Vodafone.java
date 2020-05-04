deef9986b79564d2c06bff820967548e35e315a7
SuchitaPal <62982555+Suchita-wrk@users.noreply.github.com>
2020-05-04 10:59:56 +0530

    new commit

---
+++ springioc/src/com/ioc/Vodafone.java
@@ -0,0 +1,16 @@
+package com.ioc;
+
+public class Vodafone implements Sim{
+
+	@Override
+	public void calling() {
+		System.out.println("Calling using vodafone sim.");
+	}
+
+	@Override
+	public void data() {
+		System.out.println("Browsing internet using vodafone data.");
+		
+	}
+
+}
